0b81744a788bf953b5f73bb7467657875cb10521
Sanu <hsaniya248@gmail.com>
2026-04-19 19:05:17 +0530

    Add files via upload

---
+++ Remove_by_index.java
@@ -0,0 +1,21 @@
+package arrayList;
+
+import java.util.*;
+public class Remove_by_index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("green");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+//		int n=3;
+		String Str="green";
+		System.out.println(ls);
+		System.out.println("Removing the nth Element:"+ls.remove(Str));
+		System.out.println(ls);
+	}
+
+}
