19fe025de86d3739109d4780cad44dac457aaa94
Sanu <hsaniya248@gmail.com>
2026-04-19 20:02:31 +0530

    Update ArrayListObj.java

--- ArrayListObj.java
+++ ArrayListObj.java
@@ -1,3 +1,6 @@
+/*1b(3). Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)*/
+
 package arrayList;
 
 import java.util.*;
