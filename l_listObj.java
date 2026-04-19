10837c51c2204de358407da8aa0055bca85377f4
Sanu <hsaniya248@gmail.com>
2026-04-19 19:52:15 +0530

    Add files via upload

---
+++ l_listObj.java
@@ -0,0 +1,22 @@
+package linkedList;
+
+import java.util.*;
+
+public class l_listObj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<String> ls = new LinkedList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println(ls);
+		ls.addLast("pink");
+		System.out.println(ls);
+	
+	}
+
+}
