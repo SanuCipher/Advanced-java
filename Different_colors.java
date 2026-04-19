515175c5b5e54576255cb43cc39f180c319e3fb0
Sanu <hsaniya248@gmail.com>
2026-04-19 18:05:04 +0530

    Add files via upload

---
+++ Different_colors.java
@@ -0,0 +1,20 @@
+package arrayList;
+import java.util.*;
+public class Different_colors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		System.out.println(ls);
+		if(ls.contains("red")) {
+			System.out.println("available");
+		}else {
+			System.out.println("available not");
+		}
+	}
+
+}
