b8f0ce090bc39d5dd0f6c998eef66ab66f303892
Sanu <hsaniya248@gmail.com>
2026-04-19 21:10:24 +0530

    Update IteratorObject.java

--- IteratorObject.java
+++ IteratorObject.java
@@ -1,3 +1,6 @@
+/*1c(1)). Write a Java program to iterate through all elements in a linked list starting at the
+specified position ( 2 nd ) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))*/
+
 package linkedList;
 
 
