eecca84a6603b9b4b0721b8e6a1863d09078eaaf
Sanu <hsaniya248@gmail.com>
2026-04-20 19:07:29 +0530

    Update NumericCheck.java

--- NumericCheck.java
+++ NumericCheck.java
@@ -1,3 +1,7 @@
+/*3c(8). Write a Java Program for Verifying if a string contains only numeric characters using user defined
+function isNumeric()*/
+
+
 package String_Functions;
 
 import java.util.Scanner;
